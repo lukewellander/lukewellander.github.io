618734a63758df2e249a7204f3c91591f3e016f5
lwellander18 <lwellander18@georgefox.edu>
2021-10-28 14:30:35 -0700

    added adventure to landing page

--- adventure.js
+++ adventure.js
@@ -1,12 +1,19 @@
 function clickEnter(e) {
-    if (event.keyCode == 13) {
+    if (e.keyCode == 13) {
         parseInput(e);
     }
 }
 
+/** takes the input from the text box and parses it 
+ *  so that it may be used as commans for the game
+ */
 function parseInput(e) {
     var inputArea = e.target;
     var input = inputArea.value;
 
     inputArea.value = "";
+
+    if (input != ""){
+
+    }
 }
