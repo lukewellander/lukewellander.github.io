3ce10ee7046c0018ac785ce2a4b234f24274834f
lwellander18 <lwellander18@georgefox.edu>
2021-10-28 23:11:44 -0700

    transitions working great!

--- adventure.js
+++ adventure.js
@@ -9,17 +9,23 @@ var data = {
         "startCaveEntrance" : {
             "locations": {
                 "startCave" : ["the cave", "cave"],
-                "startTown" : ["the path", "path", "town", "small town"]
+                "startTown" : ["the path", "path", "town", "small town", "the village", "the small village"]
             },
             "items": {
-                "rusty sword" : ["sword", "rusty sword"]
+                "rusty sword" : ["rusty sword"]
             },
             "inspectables" : {
                 "bush" : {
                     "keywords": ["bush", "bushes"]
                 }
             },
-            "story": "The sun blinds you as you make your way out of the cave. As your eyes begin to adjust you make out a path leading down the mountain to the east. It looks like it heads to a small village."
+            "story": "The sun blinds you as you make your way out of the cave. Your eyes begin to adjust and you can make out a path leading down the mountain side to the east. It looks like it heads to a small village."
+        },
+        "startTown": {
+            "locations": {
+                "startCaveEntrance" : ["the mountain", "the cave", "mountain", "cave"],
+            },
+            "story": "As you aproach the town you notice it is very quite you dont hear anything except the wind blowing though the trees."
         }
     },
     "commands": {
@@ -46,16 +52,20 @@ function moveTo(location) {
     displayStory(location);
 }
 
-function displayStory(location) {
+async function displayStory(location) {
     var text = data.locations[location].story;
-    var para = document.getElementById("story");
+    var display = document.getElementById("playArea");
+    var para = document.createElement("p");
 
-    para.classList.add("fade-out-text");
-    para.classList.remove("fade-int-text");
+    var oldPara = document.getElementsByClassName("fade-in-text")[0];
+    oldPara.classList.remove("fade-in-text");
+    oldPara.classList.add("fade-out-text");
+    await delay(2);
+    display.removeChild(oldPara);
 
+    display.appendChild(para);
     para.innerText = text;
     para.classList.add("fade-in-text");
-    para.classList.remove("fade-out-text");
 }
 
 
@@ -73,6 +83,7 @@ function clickEnter(e) {
             console.log(command);
             if (data.commands[command] == "move") {
                 var location = input.replace(command + " ", "");
+                console.log(location);
                 location = Object.keys(data.locations[player.location].locations).filter(loc => Object.values(data.locations[player.location].locations[loc]).some(x => x == location));
                 console.log("move to " + location);
                 moveTo(location);
@@ -105,5 +116,11 @@ function inputAcceptedAnimation(inputArea) {
 //     inputClone.style.position = "relative";
 //     inputClone.style.top = "-5.55vw";
     
+}
+
+function delay(n){
+    return new Promise(function(resolve){
+        setTimeout(resolve,n*1000);
+    });
 }
 
